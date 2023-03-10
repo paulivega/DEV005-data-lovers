1144937688c6d01805d482e0e4f8b3d6bbc36545
SandraMTJ <sandra.tabares.j@gmail.com>
2023-03-10 14:31:22 -0500

    obtención de selección de usuario en Type

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 
 // estas funciones son de ejemplo
-export const filterBug = (a) => {
-  const filterBugs = a.filter(pokemonBug=> pokemonBug.type.includes('bug'));
-  return filterBugs;
+export const filter = (a,selecType) => {
+  const filterTypes = a.filter(pokemonBug=> pokemonBug.type.includes(selecType));
+  return filterTypes;
 };
+
+//Función ordenar
+
 
 export const anotherExample = () => {
   return 'OMG';
